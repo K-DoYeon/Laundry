dab5667a581fdefb24603a45e98398c55d92dbbc
K-DoYeon <124326065+K-DoYeon@users.noreply.github.com>
2023-06-05 01:43:25 +0900

    BoardBean,DAO create / Boardlist,delete design

---
+++ src/main/java/board/BoardDAO.java
@@ -0,0 +1,182 @@
+package board;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class BoardDAO {
+	Connection con; // 데이터베이스에 접근하도록 설정
+	PreparedStatement pstmt; // 데이터베이스에서 쿼리를 실행시켜주는 객체
+	ResultSet rs; // 데이터베이스의 테이블의 결과를 리턴받아 자바에 저장해주는 객체
+	
+	// 데이터베이스에 접근
+	public void getCon() {
+		try {
+			
+			String id = "root";
+			String password = "!+La:m6V;t";
+			String url = "jdbc:mysql://3.35.8.147/laundry";
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, id, password);
+			System.out.println("DB 연결 성공");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("DB 연결 실패");
+		}
+	}
+	
+	// 하나의 새로운 게시글이 넘어와서 저장되는 메소드
+		public void insertBoard(BoardBean bean) {
+			getCon();
+			
+			try {
+				//게시글 전체값을 데이터 테이블에 저장
+				String sql = "insert into board"
+							+"(uid, upass, level, subject, content, wdate, readcount, replaycount, like)"
+							+"values"
+							+"(?, ?, ?, ?, ?, sysdate(), 0, 0, 0)";
+				pstmt = con.prepareStatement(sql);
+				
+				pstmt.setString(1, bean.getUid());
+				pstmt.setString(2, bean.getUpass());
+				pstmt.setInt(3, bean.getLevel());
+				pstmt.setString(4, bean.getSubject());
+				pstmt.setString(5, bean.getContent());
+				
+				pstmt.executeUpdate();
+				
+				con.close();
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// 모든 게시글 리턴
+		public Vector<BoardBean> getAllBoard(){
+			Vector<BoardBean> v = new Vector<>();
+			
+			getCon();
+			
+			try {
+				String sql = "select * from board order by num desc";
+				pstmt = con.prepareStatement(sql);
+				// 쿼리 실행 후 결과 저장
+				rs = pstmt.executeQuery();
+				// 데이터 개수를 모르기에 반복문으로 추출
+				while (rs.next()) {
+					// 데이터를 패키징
+					BoardBean bean = new BoardBean();			
+					bean.setNum(rs.getInt(1));
+					bean.setUid(rs.getString(2));
+					bean.setUpass(rs.getString(3));
+					bean.setLevel(rs.getInt(4));
+					bean.setSubject(rs.getString(5));
+					bean.setContent(rs.getString(6));
+					bean.setWdate(rs.getDate(7).toString());
+					bean.setReadcount(rs.getInt(8));
+					bean.setReplycount(rs.getInt(9));
+					bean.setLike(rs.getInt(10));
+					// 패키징 한 데이터를 백터에 저장
+					v.add(bean);
+				}
+				
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return v;
+		}
+		
+		public int getBoardCount() {
+			getCon();
+			int count = 0;
+			try {
+				String sql = "select count(*) from board";
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					count = rs.getInt(1);
+					System.out.println("게시판 글 갯수 : " + count);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					con.close();
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return count;
+		}
+		
+		public ArrayList<BoardBean> getBoardList(){
+			ArrayList<BoardBean> boardlist = new ArrayList<BoardBean>();
+			getCon();
+			try {
+				String sql = "select * from board order by num desc";
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				
+				while(rs.next()) {
+					BoardBean bBean = new BoardBean();
+					bBean.setNum(rs.getInt(1));
+					bBean.setUid(rs.getString(2));
+					bBean.setUpass(rs.getString(3));
+					bBean.setLevel(rs.getInt(4));
+					bBean.setSubject(rs.getString(5));
+					bBean.setContent(rs.getString(6));
+					bBean.setWdate(rs.getString(7));
+					bBean.setReadcount(rs.getInt(8));
+					bBean.setReplycount(rs.getInt(9));
+					bBean.setLike(rs.getInt(10));
+					
+					boardlist.add(bBean);
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			return boardlist;
+		}
+		
+		//페이징 처리
+		public ArrayList<BoardBean> getBoardList(int startRow, int pageSize){
+			ArrayList<BoardBean> boardlist = new ArrayList<BoardBean>();
+			getCon();
+			try {
+				String sql = "select * from board order by num desc limit ?,?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, startRow - 1);
+				pstmt.setInt(2, pageSize);
+				rs = pstmt.executeQuery();
+				while(rs.next()) {
+					BoardBean bBean = new BoardBean();
+					bBean.setNum(rs.getInt(1));
+					bBean.setUid(rs.getString(2));
+					bBean.setUpass(rs.getString(3));
+					bBean.setLevel(rs.getInt(4));
+					bBean.setSubject(rs.getString(5));
+					bBean.setContent(rs.getString(6));
+					bBean.setWdate(rs.getString(7));
+					bBean.setReadcount(rs.getInt(8));
+					bBean.setReplycount(rs.getInt(9));
+					bBean.setLike(rs.getInt(10));
+					
+					boardlist.add(bBean);
+					System.out.print(pstmt);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return boardlist;
+		}
+	
+}
--- src/main/webapp/board/boardlist.jsp
+++ src/main/webapp/board/boardlist.jsp
@@ -1,45 +1,89 @@
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.Vector"%>
+<%@page import="board.BoardDAO"%>
+<%@page import="board.BoardBean"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Laundry</title>
  <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
       integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
       crossorigin="anonymous"
     />
-<link rel="stylesheet" href="css/boardlist.css">
+<link rel="stylesheet" href="../css/boardlist.css">
 </head>
+<jsp:include page="../include/header.jsp"></jsp:include>
 <body>
 
-<h1>게시판</h1>
+<div class="do-list-title">
+	<h1>게시판</h1>
+</div>
 
 <div class="top-tab">
 	<ul class="tab">
-		<li><a href="#" class="active" data-idx="#QA">Q&A</a></li>
-		<li><a href="#" data-idx ="#notice">NOTICE</a></li>
+		<a href="#" class="active" data-idx="#QA"><li>Q&A</li></a>
+		<a href="#" data-idx ="#notice"><li>NOTICE</li></a>
 	</ul>
 </div>
+	<div class="do-container">
     <div class="faq-container">
-     <div class="active" id="QA">
+      	
+ 	<div class="active" id="QA">
       <div class="faq">
-        <h3 class="faq-title">자주 묻는 질문 1</h3>
+      	<div class="do-title">
+	      	<div class="do-day">
+	      		<span class="big">01</span>
+	      		<span>2023.10.10</span>
+	      	</div>
+	      	<div><h3 class="faq-title">이게머에요?</h3></div>
+      	</div>
 
-        <p class="faq-text">대답 1</p>
+        <p class="faq-text">이게 먼지 머르겠어여 헤헤</p>
 
         <button class="faq-toggle">
           <i class="fas fa-chevron-down"></i>
           <i class="fas fa-times"></i>
         </button>
-		</div>
-		</div>
+	   </div>
+		
+		<div class="faq">
+      	<div class="do-title">
+	      	<div class="do-day">
+	      		<span class="big">01</span>
+	      		<span>2023.10.10</span>
+	      	</div>
+	      	<div><h3 class="faq-title">이게머에요?</h3></div>
+      	</div>
+
+        <p class="faq-text">이게 먼지 머르겠어여 헤헤</p>
+
+        <button class="faq-toggle">
+          <i class="fas fa-chevron-down"></i>
+          <i class="fas fa-times"></i>
+        </button>
+	   </div>
+	   <div class="write-button">
+	        <button onclick="location.href='BoardWrite.jsp'">글쓰기</button>
+	   </div>
+	</div>
+	
+	
+
 
      <div id="notice">
       <div class="faq">
-        <h3 class="faq-title">공지사항</h3>
+        <div class="do-title">
+	      	<div class="do-day">
+	      		<span class="big">01</span>
+	      		<span>2023.10.10</span>
+	      	</div>
+	      	<div><h3 class="faq-title">공지사항</h3></div>
+      	</div>
 
         <p class="faq-text">대답 1</p>
 
@@ -47,12 +91,20 @@
           <i class="fas fa-chevron-down"></i>
           <i class="fas fa-times"></i>
         </button>
-		</div>
-		</div>
+	  </div>
+	</div>
+		
+		
+		
     </div>
     
+    <div class="">
+	        <button onclick="dodel()">삭제</button>
+	   </div>
+    </div>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="js/boardlist.js"></script>
+    <script src="../js/boardlist.js"></script>
 
 </body>
 </html>
