5585b8e49f06ead7330f84c18bc7c0cdaa3b135a
myeongjin <125331795+myeongjin123@users.noreply.github.com>
2023-06-23 16:12:25 +0900

    좋아요 기능 구현 완료.

--- src/main/java/review/ReviewDAO.java
+++ src/main/java/review/ReviewDAO.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Vector;
 
+import board.BoardBean;
+
 public class ReviewDAO {
 	   Connection con; // 데이터베이스에 접근하도록 설정
 	   PreparedStatement pstmt; // 데이터베이스에서 쿼리를 실행시켜주는 객체
@@ -53,6 +55,72 @@ public class ReviewDAO {
 			System.out.println("데베연결실패");
 		}
 	   }
+	   
+	   //게시글 삭제
+	   public int deleteReview(int num, String upass) {
+			int result = -1;
+			getCon();
+			try {
+				String sql = "select upass from review where num=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, num);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					if(upass.equals(rs.getString("upass"))) {
+						String query = "delete from review where num=?";
+						pstmt=con.prepareStatement(query);
+						pstmt.setInt(1, num);
+						result = pstmt.executeUpdate();
+					}else {
+						//비밀번호 오류 시
+						result = 0;
+					}
+				}else {
+					//게시글 없을 시
+					return -1;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+	   
+	   //게시글 수정
+	   public int reviewUpdate (ReviewBean bean) {
+			int result = 0;
+			getCon();
+			try {
+				String sql = "select upass from review where num=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setInt(1, bean.getNum());
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					if(bean.getUpass().equals(rs.getString("upass"))) {
+						//게시판에 글이 있으면 수정 가능
+						String qeury = "update review set content=? where num=?";
+						pstmt = con.prepareStatement(qeury);
+						pstmt.setString(1, bean.getContent());
+						pstmt.setInt(2, bean.getNum());
+						result = pstmt.executeUpdate();
+						System.out.println("게시글 수정 완료"+result);
+					}else {
+						//게시판 비밀번호가 다를 시
+						result = 0;
+					}
+				}else {
+					//게시판 글이 없을 시
+					result = -1;
+				}
+				System.out.println("게시글 수정 완료"+result);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+	   
+	   
 	   
 	   
 	// 모든 게시글을 리턴해주는 메소드 작성
@@ -109,7 +177,6 @@ public class ReviewDAO {
 					bean.setContent(rs.getString(5));
 					bean.setWdate(rs.getString(7));
 					// bean.setReplyCount(rs.getInt(6));
-					
 					reviewlist.add(bean);   // list에 해당 인스터스를 넘긴다
 				}
 			} catch (Exception e) {
@@ -134,6 +201,7 @@ public class ReviewDAO {
 					bean.setSubject(rs.getString(4));
 					bean.setContent(rs.getString(5));
 					bean.setWdate(rs.getString(7));
+					bean.setLike_this(rs.getInt(10));
 					// bean.setReadCount(rs.getInt(5));
 					// bean.setReplyCount(rs.getInt(6));
 					
@@ -211,6 +279,28 @@ public class ReviewDAO {
 				pstmt = null;
 				try {
 					String sql = "UPDATE review SET like_this = like_this + 1 WHERE num = ?";
+					pstmt = con.prepareStatement(sql);
+					pstmt.setInt(1, num);
+					return pstmt.executeUpdate();
+				}catch(Exception e){
+					e.printStackTrace();
+				}finally {
+					try {
+						if(pstmt != null) pstmt.close();
+						if(con != null) con.close();
+					}catch(Exception e) {
+						e.printStackTrace();
+					}
+				}
+				return -1;
+			}
+			
+			
+			public int delete(int num) {
+				getCon();
+				pstmt = null;
+				try {
+					String sql = "UPDATE review SET like_this = like_this - 1 WHERE num = ?";
 					pstmt = con.prepareStatement(sql);
 					pstmt.setInt(1, num);
 					return pstmt.executeUpdate();
